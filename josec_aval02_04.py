acc7952d128790220acc9be0eb327edec379dbf7
José Carlos <49734983+zecabanhas@users.noreply.github.com>
2019-07-05 22:34:26 -0300

    avaliacao

---
+++ josec_aval02_04.py
@@ -0,0 +1,9 @@
+dia1 = int(input("Dia 1: "))
+mes1 = int(input("Mês 1: "))
+ano1 = int(input("Ano 1: "))
+
+dia2 = int(input("Dia 2: "))
+mes2 = int(input("Mês 2: "))
+ano2 = int(input("Ano 2: "))
+
+print((ano2 - ano1) * 365 + ((mes2 - mes1) * 30) + (dia2 - dia1))
